fa3f84dd649e655588e0eae8d4d288a2eae4ccfd
Matthew Sherman <msherman0801@gmail.com>
2019-03-28 20:39:40 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,35 @@
+require 'pry'
 class Song < ActiveRecord::Base
   # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def genre_name
+    self.genre ? self.genre.name : nil
+  end
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+
+  def note_contents=(notez)
+    notez.each do |i|
+      next if i.empty?
+      self.notes.build(content: i)
+    end
+  end
+
+  def note_contents
+    self.notes.map {|i| i.content }
+  end
 end
+
